5d53abf85c7f3bf2a6dd230dc748a64fc594b583
astridholstad <astridholstad21@gmail.com>
2025-03-14 13:18:11 +0100

    update

---
+++ oblig2/server.py
@@ -0,0 +1,46 @@
+from socket import *
+import sys # In order to terminate the program
+
+#i will develop a server that handles one http req at a time
+#the web server should accept and parse a http req 
+#get the requested file from the server’s file system
+# create an HTTP response message consisting of the requested file preceded by header line
+# and then send the response directly to the client
+# if the requested file is not present in the server, the server should sendan HTTP “404 Not Found” message back to the client
+
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverPort = 5500
+serverSocket.bind(('', serverPort))
+serverSocket.listen(1)
+
+while True:
+    #etablish the connection
+    print('Ready to serve...')
+    connectionSocket, addr = serverSocket.accept()
+    try:
+        message = connectionSocket.recv(1024).decode()
+        print(f"Recieved message: {message}")
+
+        filename = message.split()[1]
+        f = open(filename[1:])
+        outputdata = f.read
+        #Send one HTTP header line into socket, 200 ok
+        header = "HTTP/1.1 200 OK \r\nContent-Type: text/html\r\n\r\n"
+        connectionSocket.send(header.encode())
+
+        #Send the content of the requested file to the client
+        for i in range(0, len(outputdata)):
+            connectionSocket.send(outputdata[i].encode())
+            connectionSocket.send("\r\n".encode())
+            connectionSocket.close()
+    except IOError:
+    #Send response message for file not found
+    header = "HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n"
+    response = "<html><head></head><body><h1>404 Not Found</h1></body></html>\r\n"
+    connectionSocket.send(header.encode())
+    connectionSocket.send(response())
+    #Close client socket
+    connectionSocket.close()
+
+    serverSocket.close()
+    sys.exit()#Terminate the program after sending the corresponding data
---
+++ oblig2/server_multi.py
@@ -0,0 +1,53 @@
+from socket import *
+import sys 
+import _thread as thread
+import time
+
+
+
+# You should implement a multithreaded server that is capable of serving multiple requests simultaneously
+# Using threading, first create a main thread in which your modified server listens for clients at a fixed port
+# When it receives a TCP connection request from a client
+#  it will set up the TCP connection through another port and services the client request in a separate thread
+# There will be a separate TCP connection in a separate thread for each request/response pair
+def timeNow()
+	"""
+	returns the time of day
+	"""
+	return time.ctime(time.time())
+
+def handleClient(connection):
+	while True:
+		message = connection.recv(1024).decode()
+		print(f"Recieved message: {message}")
+		if(message == "exit"):
+			break
+		connection.close()
+		
+		
+
+def main():
+	
+    serverSocket = socket(AF_INET, SOCK_STREAM)
+    serverPort = 12000    
+
+    try:
+	serverSocket.bind('', serverPort)
+	
+    except:
+	    print("Connection failed")
+	    sys.exit()
+		
+    serverSocket.listen(1)
+    print('Server is ready for connection')	
+	
+    while True:
+		connectionSocket, addr = serverSocket.accept()
+		print("Server connected by: ", addr)
+		print('at ', timeNow())
+		thread.start_new_thread(handleClient, (connectionSocket,))
+    serverSocket.close()
+	
+		
+if __name__ == '__main__':
+	main()
