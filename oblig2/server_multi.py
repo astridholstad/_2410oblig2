4111bbabe2672289d8f5e61ec60815804fe33ea5
astridholstad <astridholstad21@gmail.com>
2025-03-21 10:19:35 +0100

    server multi

--- oblig2/server_multi.py
+++ oblig2/server_multi.py
@@ -1,6 +1,7 @@
 from socket import *
 import sys 
-import _thread as thread
+import threading
+import os
 import time
 
 
@@ -16,37 +17,97 @@ def timeNow()
 	"""
 	return time.ctime(time.time())
 
-def handleClient(connection):
-	while True:
-		message = connection.recv(1024).decode()
-		print(f"Recieved message: {message}")
-		if(message == "exit"):
-			break
-		connection.close()
-		
-		
+def handleClient(connection_socket, addr):
+	#this method handles an http client req in its own seperate thread
+	#handles empty req
+	try:
+		message = connection_socket.recv(1024).decode()
+		print(f"Recieved message from {addr} at {timeNow()}")
+		print(f"Request: {message.splitlines()[0] if message else 'Empty request'}")
 
+		#parsing the http req to exstract get the filename
+		if not message:
+			#empty req, and closing the connection
+			connection_socket.close()
+			return
+		#get the filename from the req
+		filename = message.split()[1]
+		if filename.startwith('/'):
+			filename =filename[1:]
+
+		#if not a spesificed file requested, use the index.html file in folder
+		if filename = "":
+			filename = "index.html"
+
+		#now open and read the file
+		with open(filename, 'rb') as file:
+			response_content = file.read()
+
+		header = "HTTP/1.1 200 OK\r\n"	
+		header += f"Time: {timeNow()}\r\n"
+		header += "Server: MultiThreaded-Server\r\n"
+
+		connectionSocket.send(header.encode())
+        connectionSocket.send(response_content)
+	
+	exceptIOError:
+	#send response message if not found
+		header = "HTTP/1.1 404 Not Found\r\n"
+        header += f"Date: {timeNow()}\r\n"
+        header += "Server: MultiThreaded-Server\r\n"
+        header += "Content-Type: text/html\r\n"
+	
+	 	connectionSocket.send(header.encode())
+        connectionSocket.send(response_content.encode())
+	except Exception as e:
+        print(f"Error handling client request: {e}")
+    finally:
+        # Close the client connection
+        connectionSocket.close()
+        print(f"Connection with {addr} closed at {timeNow()}")
+
+	
+	
+	
 def main():
 	
     serverSocket = socket(AF_INET, SOCK_STREAM)
+	#allow reuse of the socket 
+	serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+	#define my port, 
     serverPort = 12000    
 
     try:
-	serverSocket.bind('', serverPort)
-	
+		serverSocket.bind('', serverPort)
+		print(f'Server startes on port {serverPort} at {timeNow()}')
     except:
 	    print("Connection failed")
 	    sys.exit()
+
+	#listen for further connections, 5 is max qued connections	
 		
-    serverSocket.listen(1)
-    print('Server is ready for connection')	
+    serverSocket.listen(5)
+    print('Server is ready for further connections')	
+
+	try: 
 	
-    while True:
-		connectionSocket, addr = serverSocket.accept()
-		print("Server connected by: ", addr)
-		print('at ', timeNow())
-		thread.start_new_thread(handleClient, (connectionSocket,))
-    serverSocket.close()
+    	while True:
+			connectionSocket, addr = serverSocket.accept()
+			print("Server connected by: {addr} at {timeNow()}")
+			#create a thread for a new connection
+			client_thread = threading.Thread(target=handleClient, args=(connectionSocket, addr))
+			client_thread.daemon = True # set as deamon so it can exist while the main thread exitst
+			client_thread.start()
+
+			print(f"Active threads: {threading.active_count()}")
+
+
+			thread.start_new_thread(handleClient, (connectionSocket,))
+    	
+	
+	exept:
+        # Close the server socket
+        serverSocket.close()
 	
 		
 if __name__ == '__main__':
